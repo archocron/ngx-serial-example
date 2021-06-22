a5794c31e84e73751bc36258db147016abb3dd49
Alejandro Rioja <archocron@gmail.com>
2021-06-21 20:53:36 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxSerial } from 'ngx-serial';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-serial-example';
+
+  serial: NgxSerial;
+
+  constructor() {
+    this.serial = new NgxSerial(this.dataHandler);
+  }
+
+  dataHandler(data: string) {
+    console.log("From arduino -> " + data);
+  }
+
+  connect() {
+    this.serial.connect();
+  }
+
+  toggleL1() {
+    this.serial.sendData(String.fromCharCode(76, 49, 10)); //L1/n
+  }
+  toggleL2() {
+    this.serial.sendData(String.fromCharCode(76, 50, 10));//L2/n
+  }
+
+  close() {
+    this.serial.close();
+  }
+
+
 }
